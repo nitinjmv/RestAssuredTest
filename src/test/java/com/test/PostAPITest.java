4470275f5299c372bf5f61f4944bc999e7660e84
nitinjmv <nitinjmv@gmail.com>
2023-03-26 17:06:43 +0100

    rename file

---
+++ src/test/java/com/test/PostAPITest.java
@@ -0,0 +1,38 @@
+package com.test;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.util.StringJoiner;
+
+public class PostAPITest {
+
+	private String endpoint = "https://jsonplaceholder.typicode.com/posts";
+
+	@Test
+	private void printAPIResponse() {
+
+		given().when().get(endpoint).then().log().all();
+	}
+
+	@Test
+	private void testAPIStatusCode() {
+		given().when().get(endpoint).then().assertThat().statusCode(200);
+	}
+	
+	@Test
+	private void testPostById() {
+		String postId = "1";
+		StringJoiner sj = new StringJoiner("/");
+		sj.add(endpoint).add(postId);
+		
+		given()
+		.when()
+		.get(sj.toString())
+		.then()
+		.body("id", is(1)); // is() because id is coming as  int in response
+		//.body("title", containsString("sunt aut facere repellat provident occaecati excepturi optio reprehenderit")); // to compare string
+	}
+
+}
